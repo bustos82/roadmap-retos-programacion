9c965c546784277be311c39a11a8acf6817b7a0e
bustos82 <113648561+bustos82@users.noreply.github.com>
2024-11-12 16:34:26 +0100

    Create bustos82.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/bustos82.py
@@ -0,0 +1,23 @@
+# https://python.org
+
+# comentario en una linea
+
+"""
+Esto tambien es un comentario
+en varias lineas
+"""
+
+my_variable = "Mi variable"
+my_variable = "Nuevo valor de mi variable"
+
+
+MY_CONSTANT = "Mi constante"
+
+my_int = 1
+my_float = 1.5
+my_bool = true
+my_string = "Mi cadena de texto"
+my_string = 'Mi cadena de texto con comillas simples'
+
+print(" Hola, python ")
+
